5e1e9733ad26af7113ea13d4300ac18b628ca344
Riddy21 <songt7@mcmaster.ca>
2020-05-17 19:48:16 -0400

    Editted Ridvan Folder

---
+++ Ridvan/fileRead.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from openpyxl.workbook import Workbook
+
+df_excel = pd.read_excel('../Exercise Files/regions.xlsx')
+df_csv = pd.read_csv('../Exercise Files/Names.csv')
+df_txt = pd.read_csv('../Exercise Files/data.txt')
+
+print(df_excel)
+print(df_txt)
